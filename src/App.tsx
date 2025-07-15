228798a5bf5ab7bd0240409131735718b4589007
Matthew Boisse <matthewboisse@Matthews-MacBook-Pro.local>
2025-07-15 10:28:03 -0500

    starting new project

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import "./App.css";
-import Form, { type FormHandle} from "./components/Form";
-import {Button} from "./components/Button";
-import  Input  from "./components/Input";
+import Form, { type FormHandle } from "./components/Form";
+import { Button } from "./components/Button";
+import Input from "./components/Input";
 import { useRef } from "react";
-
-
-
-
 
 function App() {
   const customForm = useRef<FormHandle>(null);
-  
-  function handleSave(data: unknown) { 
+
+  function handleSave(data: unknown) {
     const extractedData = data as { name: string; age: number };
     console.log(extractedData, "extractedData");
-    customForm.current?.clear()
+    customForm.current?.clear();
   }
   return (
     <main>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,47 +1,50 @@
-import { ComponentPropsWithoutRef, FormEvent, forwardRef, useImperativeHandle, useRef } from "react"
+import {
+  ComponentPropsWithoutRef,
+  FormEvent,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+} from "react";
 
 export type FormHandle = {
-    clear: () => void;
-}
+  clear: () => void;
+};
 
+type FormProps = ComponentPropsWithoutRef<"form"> & {
+  onSave: (value: unknown) => void;
+};
 
-type FormProps = ComponentPropsWithoutRef<"form">  & {
-    onSave: (value: unknown) => void;
-}
+const Form = forwardRef<FormHandle, FormProps>(function Form(
+  { onSave, children, ...otherProps },
+  ref
+) {
+  const form = useRef<HTMLFormElement>(null);
 
+  useImperativeHandle(ref, () => {
+    return {
+      clear() {
+        console.log("clear");
 
+        form.current?.reset();
+      },
+    };
+  });
 
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
 
-const Form = forwardRef<FormHandle, FormProps>( function Form({ onSave, children, ...otherProps },
-    ref) {
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData);
+    onSave(data);
 
-    const form = useRef<HTMLFormElement>(null);
-    
-    useImperativeHandle(ref, () => {
-        return {
-            clear() {
-                console.log("clear")
+    // form.current?.reset() : null
+  }
 
-                form.current?.reset()
-            }
-        }
-    })
-    
-    function handleSubmit(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault()  
+  return (
+    <form onSubmit={handleSubmit} {...otherProps} ref={form}>
+      {children}
+    </form>
+  );
+});
 
-        const formData = new FormData(event.currentTarget)
-        const data = Object.fromEntries(formData);
-        onSave(data)
-   
-        // form.current?.reset() : null
-    }
-
-    return (
-        <form onSubmit={handleSubmit}{...otherProps} ref={form}>
-            {children}
-        </form>
-    )
-})
- 
 export default Form;
